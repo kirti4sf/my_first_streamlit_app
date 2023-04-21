6868a4f26e3dd16fe93fadd9876dba513dc3b6fd
kirti4sf <131421781+kirti4sf@users.noreply.github.com>
2023-04-21 14:06:07 +0930

    Update streamlit_app.py
    Added emojis to the text and header

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,6 @@ streamlit.text('🥗 Kale, SPpinach and Rocket Smoothie')
 streamlit.text('🐔 Hard-boiled Free-Range Eggs')
 streamlit.text('🥑🍞 Avocado Toast')
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
 
