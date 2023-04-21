0bb3bb38b23fb677d5a2ef2fd7d838c5b09a7320
kirti4sf <131421781+kirti4sf@users.noreply.github.com>
2023-04-21 13:57:45 +0930

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,3 @@ import streamlit
 streamlit.title('My parents new healthy diner')
 streamlit.header('Breakfast Menu')
 
-Streamlit.text('Kale, SPpinach and Rocket Smoothie')
-Streamlit.text('Hard-boiled Free-Range Egg')
